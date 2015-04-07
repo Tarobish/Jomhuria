7e68d0c8fe2280b6ab44a8b389d47549a5b050ad
Lasse Fister <commander@graphicore.de>
2015-04-07 04:00:40 +0200

    workaround script for fontforge/sortsmill interoperation bug

---
+++ tools/removeEmptyBackgroundLayers.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+# sortsmill can't use glyphs with empty background layers in the form of:
+# Back
+# Fore
+# This just removes "Back\n" when it is present in the combination "Back\nFore"
+import os
+
+def removeEmptyBack(filename):
+    file = open(filename, 'r+')
+    content = file.read()
+    pos = content.find('Back\nFore');
+    if pos == -1:
+        return
+    content = content[:pos] + content[pos + len('Back\n'):]
+    print 'found', filename
+    file.seek(0)
+    file.truncate()
+    file.write(content)
+
+
+if __name__ == '__main__':
+    dirs = ('./sources/jomhuria.sfdir', './sources/jomhuria-latin.sfdir')
+    for directory in dirs:
+        files = [os.path.join(root, name) for root, _, files in os.walk('.')
+                            for name in files if name.endswith('.glyph')]
+        for f in files: removeEmptyBack(f)
