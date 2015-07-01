4546eb91240a52a8706ea23e60b47bccdaab9a65
Lasse Fister <commander@graphicore.de>
2015-07-01 19:12:25 +0200

    Generate RTL kerning.
    Apparently the simple kerning data from UFO is not sufficient for simple RTL kerning. We need to know if we do RTL or LTR Kerning.
    This overrides the method getFeatureRulesForPairs of ufo2fdk.kernFeatureWriter with a version that writes simple RTL kerning pairs, instead of LTR. So with a file that has exclusiveley RTL-Kerning, this works out.
    
    Thanks @khaledhosny for the help and info.
    
    The discussion can be found here: https://groups.google.com/forum/#!msg/googlefonts-discuss/kLnaUCegGjA/rjri18RMOvwJ

--- tools/getKernFeatureFromUFO.py
+++ tools/getKernFeatureFromUFO.py
@@ -1,8 +1,33 @@
 #! /usr/bin/env python
 import sys
 from defcon import Font
-from ufo2fdk.kernFeatureWriter import KernFeatureWriter
+from ufo2fdk.kernFeatureWriter import KernFeatureWriter, inlineGroupInstance
 
-font = Font(path=sys.argv[1])
-kfw = KernFeatureWriter(font)
-print kfw.write()
+
+
+class KernFeatureWriterRTL(KernFeatureWriter):
+    def getFeatureRulesForPairs(self, pairs):
+        """
+        Write pair rules to a list of strings.
+
+        You should not call this method directly.
+        """
+        rules = []
+        for (side1, side2), value in sorted(pairs.items()):
+            if not side1 or not side2:
+                continue
+            if isinstance(side1, inlineGroupInstance) or isinstance(side2, inlineGroupInstance):
+                line = "enum pos {0:s} {1:s} <{2:d} 0 {2:d} 0>;"
+            else:
+                line = "pos {0:s} {1:s} <{2:d} 0 {2:d} 0>;"
+            if isinstance(side1, inlineGroupInstance):
+                side1 = "[%s]" % " ".join(sorted(side1))
+            if isinstance(side2, inlineGroupInstance):
+                side2 = "[%s]" % " ".join(sorted(side2))
+            rules.append(line.format(side1, side2, value))
+        return rules
+
+if __name__ == '__main__':
+    font = Font(path=sys.argv[1])
+    kfw = KernFeatureWriterRTL(font)
+    print kfw.write()
