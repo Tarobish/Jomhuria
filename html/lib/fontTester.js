bb63c8a4debf1b283f2d30e3f036e440b764d6ca
Lasse Fister <commander@graphicore.de>
2015-09-16 01:40:49 +0200

    use bidi setup also on input box, wording

--- html/lib/fontTester.js
+++ html/lib/fontTester.js
@@ -13,7 +13,7 @@ define([
       , description = ['Hi, this is a small ["unhosted"](https://unhosted.org/) tool for font testing in the browser. It stores all your input in the ["#"](https://en.wikipedia.org/wiki/Fragment_identifier) part of the url as base64 encoded JSON. It works like this:'
         , '\n1. Enter your test content below.'
         , '* Copy the url from the address bar of your browser and share it with anyone or use it [to file a bug report](https://github.com/Tarobish/Jomhuria/issues).'
-        , '* Please attach a screenshot of the rendering to bug reports. This archives the broken state and enables us to compare with new versions of the font.'
+        , '* Please attach a screenshot of the rendering when reporting a bug. This archives the broken state and enables us to compare with new versions of the font.'
         ].join('\n')
       , exampleURL = '#live?eyJ2YWx1ZSI6IiMgQmlnIEhlYWRsaW5lXG4jIyMgc21hbGxlciBIZWFkbGluZVxuXG4+IEEgY29tbWVudCBhYm91dCB0aGUgZm9udCwgbGlrZSBcIlRoaXMgc2hvdWxkIGhhdmUgYSB0aWdodGVyIGtlcm5pbmdcIlxuXG5KdXN0IG5vcm1hbCB0ZXh0LiBUaGlzIGlzIFwibGVmdCB0byByaWdodFwiLCB3ZSBjYW4gYWxzbyBkbyBBcmFiaWMgXCJyaWdodC10by1sZWZ0XCIgYnV0IGJpZGlyZWN0aW9uYWwgbGFuZ3VhZ2Ugc3VwcG9ydCBpcyBub3QgdmVyeSBnb29kIGluIG1hcmtkb3duIHlldC4gV2UgZmFsbGJhY2sgdG8gaHRtbCB0byBzZXQgZXhwbGljaXQgZGlyZWN0aW9ucyB1c2luZyB0aGUgYGRpcmAgYXR0cmlidXRlIGBydGxgIGZvciBcInJpZ2h0IHRvIGxlZnRcIiBgbHRyYCBmb3IgXCJsZWZ0IHRvIHJpZ2h0XCI6XG5cbjxoMSBkaXI9XCJydGxcIj4g2K7ZiNi02YbZiNuM2LPbjCA8L2gxPlxuXG48cCBkaXI9XCJydGxcIj7YrtmI2LTZhtmI24zYs9uMINuM2Kcg2K7Yt9in2LfbjCDYqNmHINmF2LnZhtuMINiy24zYqNin2YbZiNuM2LPbjCDbjNinINmG2YjYtNiq2YYg2YfZhdix2KfZhyDYqNinINiu2YTZgiDYstuM2KjYp9uM24wg2KfYs9iqINmIINmB2LHYr9uMINqp2Ycg2KfbjNmGINmB2LHYp9uM2YbYryDYqtmI2LPYtyDYp9mIINin2YbYrNin2YUg2YXbjOKAjNqv24zYsdivINiu2YjYtCDZhtmI24zYsyDZhtin2YUg2K/Yp9ix2K/YjCDYqNmH4oCM2K7YtdmI2LUg2LLZhdin2YbbjCDaqdmHINiu2YjYtCDZhtmI24zYs9uMINit2LHZgdmHINuMINi02K7YtSDYqNin2LTYry4g2q/Yp9mH24wg2K/YsdqpINiu2YjYtNmG2YjbjNiz24wg2KjZhyDYudmG2YjYp9mGINuM2qkg2YfZhtixINmF2LTaqdmEINin2LPYqi4g2KjZhyDZhti42LEg2YXbjOKAjNix2LPYryDYqNix2KfbjCDYr9ix2qkg2Ygg2YTYsNiqINio2LHYr9mGINin2LIg2KrYrNix2KjZhyDYqNi12LHbjCDYrtmI2LTZhtmI24zYs9uMINio2KfbjNivINio2K/Yp9mG24zZhSDYrtmI2LTZhtmI24zYsyDYp9mB2LLZiNmGINio2LEg2Ybar9in2LHYtCDbjNqpINmF2KrZhtiMINiz2LnbjCDYr9in2LTYqtmHINin2KvYsduMINmH2YbYsduMINio2Kcg2KfYsdiy2LTigIzZh9in24wg2LLbjNio2KfbjNuMINi02YbYp9iu2KrbjCDYrtmE2YIg2qnZhtivLlvbsV0g2KfYsiDYp9uM2YYg2LHZiCDYrtmI2LTZhtmI24zYs9uMINio2Kcg2Ybar9in2LHYtCDYs9in2K/Zh9mUINmF2LfYp9mE2Kgg2Ygg2K3YqtinINi32LHYp9it24wg2K3YsdmI2YEg2Ygg2LXZgdit2YfigIzYotix2KfbjNuMINmF2KrZgdin2YjYqiDYp9iz2KouINmH2YXahtmG24zZhiDYp9iyINii2YbYrNin24zbjCDaqdmHINin24zZhiDZh9mG2LEg2KzZhtio2YfigIzZh9in24zbjCDYp9iyINiz2YbYqiDYsdinINiv2LEg2K/ZhCDYrtmI2K8g2K/Yp9ix2K8g2KjYp9uM2K8g2KLZhiDYsdinINiq2Kcg2K3Yr9uMINin2LIg2KrYp9uM2b7ZiNqv2LHYp9mB24wg2qnZhyDZhdio2KrZhtuMINio2LEg2KfYsdiy2LTigIzZh9in24wg2q/Ysdin2YHbjNqp24wg2YXYr9ix2YYg2Ygg2qnYp9ix2YfYp9uMINqG2KfZvtuMINin2LPYqiDZhdiq2YXYp9uM2LIg2qnYsdivLiBcbiIsImJpZGkiOiJsdHIifQ=='
       , tips = [
@@ -101,9 +101,8 @@ define([
     _p._setData = function(data) {
         // when there is more data like fontsize for example, well
         // use this function to keep it organized
-        // if(!data) data = getData('');
         this._input.value = data.value;
-        this._dirs.value = data.bidi || getSelectValue(this._dirs, 0);;
+        this._dirs.value = data.bidi || getSelectValue(this._dirs, 0);
         this._langs.value = data.lang || getSelectValue(this._langs, 0);
     };
 
@@ -120,14 +119,20 @@ define([
     };
 
     _p._render = function() {
-        var article = createElementfromMarkdown(
+        var dir = this._getBidiDirection()
+          , lang = this._getLanguage()
+          , article = createElementfromMarkdown(
                 'article'
               , {
-                    dir: this._getBidiDirection()
-                  , lang: this._getLanguage()
+                    dir: dir
+                  , lang: lang
                 }
               , this._input.value
             );
+        // Change input as well, otherwise non latin users could be impaired.
+        this._input.setAttribute('dir', dir)
+        this._input.setAttribute('lang', lang)
+
         this._usercontent.parentNode.replaceChild(article,  this._usercontent);
         this._usercontent = article;
     };
